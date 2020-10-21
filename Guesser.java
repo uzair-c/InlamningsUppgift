da7da36da4ec9beb044893d250c95d4734a87124
uzair-c <uzair_74@hotmail.com>
2020-10-21 10:23:53 +0200

    Done with start method

--- Guesser.java
+++ Guesser.java
@@ -35,8 +35,8 @@ public class Guesser{
    * rules() method, next the doGuesses() method.
    */
   public void start(){
-    // call the rules method here
-    // call the doGuesses() method here
+    rules();
+    doGuesses();
   }
 
   private void rules(){
